9f9661a601eac2fee416367542106f90ebb6781f
Pablo D Limardo <pablo@MacBook-Pro.local>
2022-03-20 12:49:04 -0300

    Trabajos en C++

---
+++ prueba.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numeroUno;
+    int numeroDos;
+    int resultado;
+    cout << "Ingresá el primer nuemro: " << endl << endl;
+    cin >> numeroUno;
+    cout << "Ingresa el segundo nuemro: " << endl;
+    cin >> numeroDos;
+    resultado = numeroUno + numeroDos;
+    cout << "El resultado de la suma es: " << resultado;
+    return 0;
+
+}
